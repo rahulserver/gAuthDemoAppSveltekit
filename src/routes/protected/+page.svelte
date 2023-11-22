98f46ef85afc9d87aa4d7733c209d2a59071fbdc
rahulserver <rahulserver@gmail.com>
2023-11-22 21:40:40 +0530

    Add protected route

---
+++ src/routes/protected/+page.svelte
@@ -0,0 +1,19 @@
+// svelte template code
+<script>
+  import { onMount } from 'svelte';
+  import {page} from "$app/stores";
+  import {goto} from "$app/navigation";
+
+  const store = useStore();
+
+  onMount(() => {
+    if(!$page.data?.session) {
+      goto('/');
+      return;
+    }
+  });
+</script>
+{#if $page.data?.session}
+    <div>Protected</div>
+{/if}
+
